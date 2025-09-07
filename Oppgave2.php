4eb9e831934ea1b9b4179d05c9222de530f32533
hamadaredaazab-alt <hamadareda.azab@gmail.com>
2025-09-07 12:24:37 +0200

    2o

--- Oppgave2.php
+++ Oppgave2.php
@@ -1,4 +1,4 @@
-<?php /* Oppgave 2 */
+<?php /* Oppgave2 */
 /*
 /* Programmet mottar fra et HTML-skjema et navn og en alder ved POST-metoden
 /* Programmet skriver ut en "god dag"-melding med personens navn og alder
