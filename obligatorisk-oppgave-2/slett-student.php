bc3fb01add119ce0836c0d5a8bb8ea1ab08977d1
hamadaredaazab-alt <hamadareda.azab@gmail.com>
2025-10-03 11:32:41 +0200

    22

--- obligatorisk-oppgave-2/slett-student.php
+++ obligatorisk-oppgave-2/slett-student.php
@@ -8,34 +8,34 @@
 <h3>Slett student</h3>
 
 <form method="post" action="slett-student.php" id="slettStudentSkjema" name="slettStudentSkjema">
-    Studentnummer: 
-    <input type="text" id="studentnr" name="studentnr" required /> <br/><br/>
+    Brukernavn: 
+    <input type="text" id="Brukernavn" name="Brukernavn" required /> <br/><br/>
     <input type="submit" value="Slett student" name="slettStudentKnapp" /> 
     <input type="reset" value="Nullstill" /> 
 </form>
 
 <?php
 if (isset($_POST["slettStudentKnapp"])) {
-    $studentnr = $_POST["studentnr"]; 
+    $Brukernavn = $_POST["Brukernavn"]; 
 
-    if (!$studentnr) {
-        print("Ingen studentnummer skrevet inn <br />");
+    if (!$Brukernavn) {
+        print("Ingen Brukernavn skrevet inn <br />");
     } else {
         include("db-tilkobling.php");
 
         // Sjekk om studenten finnes
-        $sjekk = "SELECT * FROM student WHERE studentnr='$studentnr';";
+        $sjekk = "SELECT * FROM student WHERE Brukernavn='$Brukernavn';";
         $resultat = mysqli_query($db, $sjekk);
 
         if (mysqli_num_rows($resultat) == 0) {
-            print("Ingen student med nummer $studentnr ble funnet.");
+            print("Ingen student med Brukernavn $Brukernavn ble funnet.");
         } else {
             // Slett studenten
-            $sqlSetning = "DELETE FROM student WHERE studentnr='$studentnr';";
+            $sqlSetning = "DELETE FROM student WHERE Brukernavn='$Brukernavn';";
             mysqli_query($db, $sqlSetning) or die("Ikke mulig å slette student fra databasen");
 
             if (mysqli_affected_rows($db) > 0) {
-                print("Student med nummer $studentnr er nå slettet.");
+                print("Student med Brukernavn $Brukernavn er nå slettet.");
             } else {
                 print("Noe gikk galt ved sletting.");
             }
