0e4c4f719a70d2f59881626f9b294a59befa6379
hamadaredaazab-alt <hamadareda.azab@gmail.com>
2025-10-07 14:35:11 +0200

    nn

--- obligatorisk-oppgave-2/dynamisk.php
+++ obligatorisk-oppgave-2/dynamisk.php
@@ -1,38 +1,52 @@
-<?php /* dynamiske-funksjoner */
+<?php  /*  dynamiske funksjoner */
 /*
-/* denne filen inneholder følgende dynamiske funksjoner:
-/* listeboksklassekode ()
+/*  denne filen inneholder følgende dynamiske funksjoner:
+/*    listeboksklassekode()
+/*    sjekkbokserklassenavn()
+*/
 
-*/
-function listeboksklassekode ()
+
+function listeboksklassekode()
 {
-include("db-tilkobling.php"); /* tilkobling til database-server og valg av database utført */
-$sqlSetning="SELECT * FROM studium ORDER BY klassekode;";
-$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-$antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
-for ($r=1;$r<=$antallRader;$r++)
+  include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
+      
+  $sqlSetning="SELECT * FROM registrer-klasse ORDER BY klassekode;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
+    /* SQL-setning sendt til database-serveren */
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
+      $postnr=$rad["klassekode"]; 
+      $poststed=$rad["klassenavn"];
+
+      print("<option value='$klassekode'>$klassekode $klassenavn</option>");  /* ny verdi i listeboksen laget */
+    }
+}
+
+function sjekkbokserbruknavn()
 {
-$rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-$klassekode=$rad["klassekode"];
-$studiumkode=$rad["studiumkode"];
-print("<option value='$studiumkode'>$studiumkode $studiumkode </option>"); /* ny verdi i listeboksen
-laget */
+  include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
+      
+  $sqlSetning="SELECT * FROM registrer-student ORDER BY fornavn;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");  
+    /* SQL-setning sendt til database-serveren */
+      
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
+      $postnr=$rad["brukernavn"];       
+      $poststed=$rad["fornavn"];    
+
+      print("<input type='checkbox' id='brukernavn' name='brukernavn[]' value='brukernavn' /> $brukernavn $fornavn <br/>");  
+        /* ny sjekkboks laget */
+    }
 }
-}
-function listeboksstudent ()
-{
-include("db-tilkobling.php"); /* tilkobling til database-server og valg av database utført */
-$sqlSetning="SELECT * FROM emne ORDER BY studentkode;";
-$sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-$antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
-for ($r=1;$r<=$antallRader;$r++)
-{
-$rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-$Brukernavn=$rad["Brukernavn"];
-$Brukernavn=$rad["Brukernavn"];
-$studiumkode=$rad["studiumkode"];
-print("<option value='$Brukernavn'>$Brukernavn $Brukernavn </option>"); /* ny verdi i listeboksen laget
-*/
-}
-}
+
+
+
 ?>
