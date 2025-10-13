a37a8dae5c3999d288caf2b17226e9c646e2addb
hamadaredaazab-alt <hamadareda.azab@gmail.com>
2025-10-13 12:56:41 +0200

    12345

--- obligatorisk-oppgave-2/dynamiske-funksjoner.php
+++ obligatorisk-oppgave-2/dynamiske-funksjoner.php
@@ -26,25 +26,25 @@ function listeboksklassekode()
     }
 }
 
-function sjekkbokserbruknavn()
+
+function sjekkbokserbruknavne()
 {
   include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
       
-  $sqlSetning="SELECT * FROM student ORDER BY fornavn;";
-  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");  
+  $sqlSetning="SELECT * FROM student ORDER BY brukernavn;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
     /* SQL-setning sendt til database-serveren */
-      
+	
   $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
   for ($r=1;$r<=$antallRader;$r++)
     {
       $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
-       $brukernavn = htmlspecialchars($rad["brukernavn"]);
-        $fornavn = htmlspecialchars($rad["fornavn"]);
-        $etternavn = htmlspecialchars($rad["etternavn"]);
-      print("<input type='checkbox' id='brukernavn' name='brukernavn[]' value='$brukernavn' id='bruker_$brukernavn' />
-              $fornavn $etternavn ($brukernavn) <br/>");  
-        /* ny sjekkboks laget */
+       $klassekode = htmlspecialchars($rad["brukernavn"]);
+        $klassenavn = htmlspecialchars($rad["fornavn"]);
+
+      print("<option value='$brukernavn'>$brukernavn $fornavn</option>");  /* ny verdi i sjekkbokserbruknavne laget */
+    }
     }
 }
 
